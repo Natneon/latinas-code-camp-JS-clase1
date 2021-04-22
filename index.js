fa7f2744509e3008607dbb04fbd017f32747dbd4
Natalia Arrieta <45863177+Natneon@users.noreply.github.com>
2021-04-21 21:28:06 -0500

    Add files via upload
    Primer reto de JavaScript

---
+++ index.js
@@ -0,0 +1,29 @@
+//STRING - Texto plano
+var verdura = "brocoli";
+var bebida = "jugo de naranja"
+
+//NUMBER - Numero
+var edad = 36;
+var diaDeNacimiento = 21;
+let mesDeNacimiento = 9;
+
+//BOOLEAN
+var laVidaEsBella = true;
+var soyUnGato = false;
+
+//UNDEFINED
+var horaDesayuno = undefined;
+
+//NULL
+var jugoDeNaranja = null;
+
+//ARRAY - arreglo
+let queVamosAComer = ["sopa", "guisado", "postre", "agua", true];
+
+//OBJECT
+var maleta = {
+  tamaño: "mediano",
+  color: "azul",
+  unicaPieza: false
+}
+
